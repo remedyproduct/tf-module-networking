e35a9921a8ff27b6fc98237b86b18094ea95db02
Sergey Kuzmich <contact@kuzmi.ch>
2021-06-24 11:02:53 +0300

    Update input types

--- _variables.tf
+++ _variables.tf
@@ -22,19 +22,19 @@ variable "vpc_cidr_block" {
 }
 
 variable "availability_zones" {
-  type        = string
+  type        = list(string)
   description = "Availability Zones within AWS Region network should be covered by"
   default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_subnets_cidrs" {
-  type        = string
+  type        = list(string)
   description = "CIDR block for Public Subnets. Number of blocks must match the number of Availability Zones"
   default     = ["192.168.0.0/19", "192.168.32.0/19"]
 }
 
 variable "private_subnets_cidrs" {
-  type        = string
+  type        = list(string)
   description = "CIDR block for Private Subnets. Number of blocks must match the number of Availability Zones"
   default     = ["192.168.128.0/19", "192.168.160.0/19"]
 }
