f44b593b4f79c6f2164c087a588d067cfdb5703c
Sergey Kuzmich <contact@kuzmi.ch>
2021-06-24 11:00:54 +0300

    Update input names

--- _variables.tf
+++ _variables.tf
@@ -27,13 +27,13 @@ variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b"]
 }
 
-variable "public_subnets_cidrs_per_availability_zone" {
+variable "public_subnets_cidrs" {
   type        = string
   description = "CIDR block for Public Subnets. Number of blocks must match the number of Availability Zones"
   default     = ["192.168.0.0/19", "192.168.32.0/19"]
 }
 
-variable "private_subnets_cidrs_per_availability_zone" {
+variable "private_subnets_cidrs" {
   type        = string
   description = "CIDR block for Private Subnets. Number of blocks must match the number of Availability Zones"
   default     = ["192.168.128.0/19", "192.168.160.0/19"]
