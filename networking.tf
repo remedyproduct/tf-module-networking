86fc4ebedcd8d5a1c13c027e4a2efe1c1e955bcf
Sergey Kuzmich <contact@kuzmi.ch>
2021-07-07 17:02:56 +0300

    Fix variable names. Use default region

--- networking.tf
+++ networking.tf
@@ -1,15 +1,17 @@
+data "aws_region" "current" {}
+
 module "networking" {
   source  = "cn-terraform/networking/aws"
   version = "2.0.3"
 
   name_preffix = substr(format("%s-%s", var.name, var.environment), 0, 32)
 
-  region  = var.region
+  region  = data.aws_region.current.name
   profile = "aws_profile"
 
 
   vpc_cidr_block                              = var.vpc_cidr_block
   availability_zones                          = var.availability_zones
-  public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs_per_availability_zone
-  private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs_per_availability_zone
+  public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
+  private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
 }
